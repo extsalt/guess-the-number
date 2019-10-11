9ba03381fdcd246e8ab7b592fd0b8c5c26846fc8
Ganesh K <ganeshk.work@gmail.com>
2019-10-11 22:38:40 +0530

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+extern crate rand;
+
 use std::io;
 
+use rand::Rng;
 fn main() {
     println!("Guess the number");
 
@@ -7,8 +10,13 @@ fn main() {
 
     let mut guess = String::new(); //create new string variable
 
+    //read user input
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    let random = rand::thread_rng().gen_range(1,100);
+
+    println!("Secret Number: {}", random);
 }
