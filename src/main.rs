bacf081440139722823821fff62a85be28ff2614
Ganesh K <ganeshk.work@gmail.com>
2019-10-11 23:38:09 +0530

    save

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,27 @@ fn main() {
 
     println!("Please enter your guess:");
 
-    let mut guess = String::new(); //create new string variable
+    loop {
+        let mut guess = String::new(); //create new string variable
+        //read user input
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    //read user input
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        //parse string to appropriate numerical value
+        let guess: u32 = guess.trim().parse()
+            .expect("Invalid #");
 
-    //parse string to appropriate numerical value
-    let guess: u32 = guess.trim().parse()
-        .expect("Please enter number");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
+        //generate random number
+        let random = rand::thread_rng().gen_range(1, 100);
 
-    //generate random number
-    let random = rand::thread_rng().gen_range(1, 100);
+        println!("Secret Number: {}", random);
 
-    println!("Secret Number: {}", random);
-
-    match guess.cmp(&random) {
-        Ordering::Less => println!("Small"),
-        Ordering::Greater => println!("Greater"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&random) {
+            Ordering::Less => println!("Small"),
+            Ordering::Greater => println!("Greater"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
